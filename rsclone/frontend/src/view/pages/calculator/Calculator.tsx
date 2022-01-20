6eb16513e594e2c052f698df009351cf92f447ce
Your Name <you@example.com>
2022-01-20 21:17:00 +0300

    refactor: add code

--- rsclone/frontend/src/view/pages/calculator/Calculator.tsx
+++ rsclone/frontend/src/view/pages/calculator/Calculator.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable react/button-has-type */
-import React from 'react';
+import React, { useReducer } from 'react';
 import './calculator.scss';
 
 // function reducer (state, action): void{}
 
 export const Calculator = () => {
-  // const [state, dispatch] = useReducer(reducer, {});
+  const [{currentOperand, previousOperand, operation},dispatch] = useReducer(reducer,{});
   return (
     <section className='calculator'>
       <div className='container'>
@@ -15,24 +14,24 @@ export const Calculator = () => {
             <div className='previous-operand' />
             <div className='current-operand' />
           </div>
-          <button className='span-two btn-pink'>AC</button>
-          <button className='btn-del'>DEL</button>
-          <button className='btn-pink'>/</button>
-          <button className='btn-num'>1</button>
-          <button className='btn-num'>2</button>
-          <button className='btn-num'>3</button>
-          <button className='btn-pink'>x</button>
-          <button className='btn-num'>4</button>
-          <button className='btn-num'>5</button>
-          <button className='btn-num'>6</button>
-          <button className='btn-pink'>+</button>
-          <button className='btn-num'>7</button>
-          <button className='btn-num'>8</button>
-          <button className='btn-num'>9</button>
-          <button className='btn-pink'>-</button>
-          <button className='btn-pink'>.</button>
-          <button className='btn-num'>0</button>
-          <button className='span-two btn-pink'>=</button>
+          <button className='span-two btn-pink' type='button'>AC</button>
+          <button className='btn-del' type='button'>DEL</button>
+          <button className='btn-pink' type='button'>/</button>
+          <button className='btn-num' type='button'>1</button>
+          <button className='btn-num' type='button'>2</button>
+          <button className='btn-num' type='button'>3</button>
+          <button className='btn-pink' type='button'>x</button>
+          <button className='btn-num' type='button'>4</button>
+          <button className='btn-num' type='button'>5</button>
+          <button className='btn-num' type='button'>6</button>
+          <button className='btn-pink' type='button'>+</button>
+          <button className='btn-num' type='button'>7</button>
+          <button className='btn-num' type='button'>8</button>
+          <button className='btn-num' type='button'>9</button>
+          <button className='btn-pink' type='button'>-</button>
+          <button className='btn-pink' type='button'>.</button>
+          <button className='btn-num' type='button'>0</button>
+          <button className='span-two btn-pink' type='button'>=</button>
         </div>
       </div>
     </section>
