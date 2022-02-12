f059c0e664cb9bc70cedb387c0e65961210da8f1
Your Name <you@example.com>
2022-02-12 03:56:07 +0300

    refactor: add api

--- rsclone/frontend/src/view/components/converter/Currencyrow.tsx
+++ rsclone/frontend/src/view/components/converter/Currencyrow.tsx
@@ -1,12 +1,18 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
-export default function Currencyrow() {
+interface IArrayConvert {
+  currencyOptions: Array<string>
+}
+
+export default function Currencyrow({ currencyOptions }:IArrayConvert) {
   return (
     <div className='currency-row'>
       <input className='currency-input' type='number' />
-      <select className='currency-select' name=''>
-        <option value='1'>1</option>
+      <select className='currency-select'>
+        {currencyOptions.map((option) => (
+          <option value={option} key={currencyOptions.indexOf(option)}>{option}</option>
+        ))}
       </select>
     </div>
   );
--- rsclone/frontend/src/view/pages/converter/Converter.tsx
+++ rsclone/frontend/src/view/pages/converter/Converter.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Currencyrow from '../../components/converter/Currencyrow';
 import { Liveconvert } from '../../components/converter/Liveconvert';
-import { Userconvert } from '../../components/converter/Userconvert';
+// import { Userconvert } from '../../components/converter/Userconvert';
 import './converter.scss';
 
 export interface ICurrentArray {
@@ -11,39 +12,49 @@ export interface ICurrent {
   Cur_Abbreviation: string,
   Cur_OfficialRate:number,
 }
-const arrCurrencyName:string[] = [];
-export const Converter = () => {
+
+export const Converter:React.FC = () => {
+  const arrCurrencyName:string[] = [];
+
   const todayDate = new Date().toLocaleString('en-US', {
     day: '2-digit', weekday: 'short', month: 'long',
   });
 
-  const [currencyOptions, setCurrencyOptions] = useState([]);
-
+  const [currencyOptions, setCurrencyOptions] = useState<string[]>([]);
+  console.log(currencyOptions);
+  const [fromCurrency, setFromCurrency] = useState<string>();
+  const [toCurrency, setToCurrency] = useState<string>();
   const BASE_URL = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
 
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data:Array<ICurrent>) => {
-        console.log(data);
-        setCurrencyOptions([arrCurrencyName]);
-        // data.forEach((el) => {
-        //   setCurrencyOptions([arrCurrencyName, ...Object.keys(el.Cur_Abbreviation)];
-        //   return arrCurrencyName;
-        // });
-        console.log(arrCurrencyName);
+        data.forEach((el) => {
+          arrCurrencyName.push(el.Cur_Abbreviation);
+        });
+        arrCurrencyName.unshift('BYN');
+        const firstCurrency = Object.keys(arrCurrencyName)[arrCurrencyName.length - 1];
+        setCurrencyOptions(arrCurrencyName);
+        setFromCurrency(arrCurrencyName[arrCurrencyName.length - 1]);
+        setToCurrency(firstCurrency);
       });
   }, []);
-
-  // useEffect(() => {
-
-  // });
   return (
     <section className='converter'>
       <div className='container'>
         <article className='converter-content'>
           <h3 className='date'>{todayDate}</h3>
-          <Userconvert />
+          <div className='user-convert'>
+            <h3>Converter</h3>
+            <Currencyrow
+              currencyOptions={currencyOptions}
+            />
+            <div className='equals'>=</div>
+            <Currencyrow
+              currencyOptions={currencyOptions}
+            />
+          </div>
           <Liveconvert />
         </article>
       </div>
