a6c2d19ac4dbd7aa1055f84980cf64b96de9cd49
Your Name <you@example.com>
2022-02-12 11:46:01 +0300

    Temporary commit

--- rsclone/frontend/src/view/components/converter/Currencyrow.tsx
+++ rsclone/frontend/src/view/components/converter/Currencyrow.tsx
@@ -1,15 +1,19 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
+import { type } from 'os';
 import React from 'react';
 
-interface IArrayConvert {
-  currencyOptions: Array<string>
+interface IProps {
+  currencyOptions: Array<string>,
+  selectedCurrency: string;
+  onChangeCurrency:React.ChangeEventHandler<HTMLSelectElement>;
 }
-
-export default function Currencyrow({ currencyOptions }:IArrayConvert) {
+export default function Currencyrow(
+  { currencyOptions, selectedCurrency, onChangeCurrency }:IProps,
+) {
   return (
     <div className='currency-row'>
       <input className='currency-input' type='number' />
-      <select className='currency-select'>
+      <select className='currency-select' value={selectedCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option) => (
           <option value={option} key={currencyOptions.indexOf(option)}>{option}</option>
         ))}
--- rsclone/frontend/src/view/pages/converter/Converter.tsx
+++ rsclone/frontend/src/view/pages/converter/Converter.tsx
@@ -8,23 +8,26 @@ export interface ICurrentArray {
   data: Array<ICurrent>;
 }
 export interface ICurrent {
-  Cur_ID:number,
   Cur_Abbreviation: string,
   Cur_OfficialRate:number,
 }
 
 export const Converter:React.FC = () => {
-  const arrCurrencyName:string[] = [];
+  const BASE_URL = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
 
   const todayDate = new Date().toLocaleString('en-US', {
     day: '2-digit', weekday: 'short', month: 'long',
   });
-
+  const arrCurrencyName:string[] = [];
+  const arrCurrencyRates:ICurrent[] = [];
   const [currencyOptions, setCurrencyOptions] = useState<string[]>([]);
   console.log(currencyOptions);
-  const [fromCurrency, setFromCurrency] = useState<string>();
-  const [toCurrency, setToCurrency] = useState<string>();
-  const BASE_URL = 'https://www.nbrb.by/api/exrates/rates?periodicity=0';
+  const [fromCurrency, setFromCurrency] = useState<string>('');
+  const [toCurrency, setToCurrency] = useState<string>('');
+  const [exchangeRate, setExchangeRate] = useState<string>();
+  const [amount, setAmount] = useState<number>(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState<boolean>(true);
+  console.log(exchangeRate);
 
   useEffect(() => {
     fetch(BASE_URL)
@@ -32,12 +35,21 @@ export const Converter:React.FC = () => {
       .then((data:Array<ICurrent>) => {
         data.forEach((el) => {
           arrCurrencyName.push(el.Cur_Abbreviation);
+          const obj = {
+            name: el.Cur_Abbreviation,
+            amount: el.Cur_OfficialRate,
+          };
+          arrCurrencyRates.push(obj);
         });
+        console.log(arrCurrencyRates);
         arrCurrencyName.unshift('BYN');
-        const firstCurrency = Object.keys(arrCurrencyName)[arrCurrencyName.length - 1];
+        const firstCurrency = Object.keys(arrCurrencyName)[1];
+        // select option
         setCurrencyOptions(arrCurrencyName);
-        setFromCurrency(arrCurrencyName[arrCurrencyName.length - 1]);
-        setToCurrency(firstCurrency);
+        setFromCurrency(firstCurrency);
+        setToCurrency(arrCurrencyName[6]);
+        console.log(data);
+        // setExchangeRate(firstCurrency[6]);
       });
   }, []);
   return (
@@ -49,10 +61,15 @@ export const Converter:React.FC = () => {
             <h3>Converter</h3>
             <Currencyrow
               currencyOptions={currencyOptions}
+              selectedCurrency={fromCurrency}
+              onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+
             />
             <div className='equals'>=</div>
             <Currencyrow
               currencyOptions={currencyOptions}
+              selectedCurrency={toCurrency}
+              onChangeCurrency={(e) => setToCurrency(e.target.value)}
             />
           </div>
           <Liveconvert />
