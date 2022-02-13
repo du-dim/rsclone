138a6530e680848935b5b83026dbff4f0cf7f81a
Your Name <you@example.com>
2022-02-14 02:33:31 +0300

    init: create components filters

--- rsclone/frontend/src/view/pages/sort/Sort.tsx
+++ rsclone/frontend/src/view/pages/sort/Sort.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import './sort.scss';
 
 export const Sort = () => {
-  return <div className='page'>Sort</div>;
+  return (
+    <section className='calculator'>
+      <div className='container'>
+        <h3>Sort</h3>
+        <nav className='chart-link'>
+          <NavLink to='/Sorts/dates' className='nav-link'><span>Date</span></NavLink>
+          <NavLink to='/Sorts/categoties-expense' className='nav-link'><span>Expense</span></NavLink>
+          <NavLink to='/Sorts/categoties-income' className='nav-link'><span>Income</span></NavLink>
+        </nav>
+        <Routes>
+          <Route path='/dates' element={<SortDates />} />
+          <Route path='categoties-expense' element={2} />
+          <Route path='categoties-income' element={3} />
+        </Routes>
+      </div>
+    </section>
+  );
 };
